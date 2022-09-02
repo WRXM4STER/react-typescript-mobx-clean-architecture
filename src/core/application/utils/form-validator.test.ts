73f975b8e49946ef73c7c76b72fe61804b6f8527
WRXM4STER <montaev.a@mail.ru>
2022-09-02 21:55:29 +0300

    added test for form-validator

---
+++ src/core/application/utils/form-validator.test.ts
@@ -0,0 +1,26 @@
+import {describe, expect, test} from '@jest/globals';
+import FormValidator from './form-validator';
+
+describe('form-validate', () => {
+
+    it('email validate is true', () => {
+      expect(FormValidator.isEmailValid('example@mail.ru')).toBe(true);
+    });
+
+    it('email validate is false', () => {
+        expect(FormValidator.isEmailValid('example@mailru')).toBe(false);
+        expect(FormValidator.isEmailValid('examplemailru')).toBe(false);
+        expect(FormValidator.isEmailValid('example')).toBe(false);
+    });
+
+    it('phone validate is true', () => {
+        expect(FormValidator.isPhoneValid('+79998887766')).toBe(true);
+    });
+
+    it('phone validate is false', () => {
+        expect(FormValidator.isPhoneValid('+7999888dfbfbdf')).toBe(false);
+        expect(FormValidator.isPhoneValid('sddsfdsf')).toBe(false);
+        expect(FormValidator.isPhoneValid('6511fs')).toBe(false);
+    });
+
+  });
